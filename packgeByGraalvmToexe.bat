dc679eb296e09a9a3d211f8c8e3f5a4af8047d30
dchg43 <75658553@qq.com>
2025-10-14 15:01:01 +0800

    增加编译为exe文件时的系统dpi缩放感知

--- packgeByGraalvmToexe.bat
+++ packgeByGraalvmToexe.bat
@@ -3,6 +3,7 @@
 cd /D "%~dp0"
 set "BASEDIR=%cd%"
 
+set APP_NAME=rubikAutoPlayer
 set "srcDir=src"
 set "destDir=%BASEDIR%\dest"
 set "libDir=lib"
@@ -14,7 +15,7 @@ set "JAR=%graalvmPath%\bin\jar"
 set "NATIVE=%graalvmPath%\bin\native-image.cmd"
 
 
-set "CLASSPATH=%BASEDIR%\conf;%BASEDIR%\rubikAutoPlayer.jar"
+set "CLASSPATH=%BASEDIR%\conf;%BASEDIR%\%APP_NAME%.jar"
 SETLOCAL ENABLEDELAYEDEXPANSION
 for %%I in ("lib\*.jar") do (
     set "CLASSPATH=!CLASSPATH!;%%I"
@@ -43,14 +44,14 @@ if not "%errorlevel%" == "0" (
 del /f /q "%destDir%"\srclist.txt
 
 :: clean jar
-if exist "%BASEDIR%\rubikAutoPlayer.jar" (
-    del /f /q "%BASEDIR%\rubikAutoPlayer.jar"
+if exist "%BASEDIR%\%APP_NAME%.jar" (
+    del /f /q "%BASEDIR%\%APP_NAME%.jar"
 )
 
 :: jar
 xcopy /e /q /y "%BASEDIR%\META-INF\native-image\" "%destDir%\META-INF\native-image\"
 echo "jar cfm FileSync.jar META-INF/MANIFEST.MF -C %destDir% com"
-"%JAR%" cfm "%BASEDIR%\rubikAutoPlayer.jar" "%BASEDIR%\META-INF\MANIFEST.MF" -C "%destDir%" .
+"%JAR%" cfm "%BASEDIR%\%APP_NAME%.jar" "%BASEDIR%\META-INF\MANIFEST.MF" -C "%destDir%" .
 if not "%errorlevel%" == "0" (
     pause
     exit /b %errorlevel%
@@ -62,7 +63,7 @@ del /f /q "%BASEDIR%\META-INF\native-image\.lock" 2>nul
 for /f "delims=" %%I in ('dir /B "%BASEDIR%\META-INF\native-image\"^|findstr "agent-pid"') do (
     rmdir /s /q "%BASEDIR%\META-INF\native-image\%%I"
 )
-start %JAVA% -Dfile.encoding=utf-8 -agentlib:native-image-agent=config-merge-dir="%BASEDIR%\META-INF\native-image" -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Duser.language=en -Duser.region=US -jar "%BASEDIR%\rubikAutoPlayer.jar" --display true -backgroundImage "%systemroot%\Web\Wallpaper\Windows\img0.jpg"
+start %JAVA% -Dfile.encoding=utf-8 -agentlib:native-image-agent=config-merge-dir="%BASEDIR%\META-INF\native-image" -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Duser.language=en -Duser.region=US -jar "%BASEDIR%\%APP_NAME%.jar" --display true -backgroundImage "%systemroot%\Web\Wallpaper\Windows\img0.jpg"
 
 rem 等待一段时间并结束进程
 timeout /nobreak /T 5 >nul
@@ -71,7 +72,7 @@ taskkill /fi "IMAGENAME eq java.exe" >nul
 
 
 ::jpackage --type app-image --name spring --input target --main-jar spring-1.0.jar --win-console --dest dist
-::jpackage --input . --name rubikAutoPlayer --main-jar rubikAutoPlayer.jar --win-console --win-shortcut
+::jpackage --input . --name %APP_NAME% --main-jar %APP_NAME%.jar --win-console --win-shortcut
 
 timeout /nobreak /T 1 >nul
 del /f /q "%BASEDIR%\META-INF\native-image\.lock" 2>nul
@@ -95,20 +96,28 @@ call "%NATIVE%" "--no-fallback" ^
     "-H:ConfigurationFileDirectories=%BASEDIR%\META-INF\native-image" ^
     "-H:+ReportExceptionStackTraces" ^
     "-H:+AddAllCharsets" ^
-    "-H:Name=rubikAutoPlayer" ^
+    "-H:Name=%APP_NAME%" ^
     "--enable-url-protocols=http,https" ^
     "--install-exit-handlers" ^
     "--link-at-build-time" ^
     "--enable-preview" ^
     "--verbose" ^
     "-O1" ^
-    "-jar" "%BASEDIR%\rubikAutoPlayer.jar"
+    "-jar" "%BASEDIR%\%APP_NAME%.jar"
+if not "%errorlevel%" == "0" (
+    pause
+    exit /b %errorlevel%
+)
 
 ::复制依赖文件
-del /f /q "%destDir%\rubikAutoPlayer.build_artifacts.txt" >nul 2>nul
+del /f /q "%destDir%\%APP_NAME%.build_artifacts.txt" >nul 2>nul
 mkdir "%destDir%\lib\"
 copy "%BASEDIR%\lib\fontconfig.bfc" "%destDir%\lib\"
 
 ::去除命令行窗口
-editbin /subsystem:windows "%destDir%\rubikAutoPlayer.jar" 2>nul
+::editbin /subsystem:windows "%destDir%\%APP_NAME%.jar" 2>nul
+
+::增加系统dpi缩放感知
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\mt.exe" -manifest "%BASEDIR%\META-INF\rubikAutoPlayer.exe.manifest" -outputresource:"%destDir%\%APP_NAME%.exe;#1"
+
 
