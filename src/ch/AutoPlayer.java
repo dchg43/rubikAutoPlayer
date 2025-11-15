36117b289e3e1aa37bf9b4e6d8e9d6a0eee1440e
dchg43 <75658553@qq.com>
2025-11-15 14:39:21 +0800

    modify

--- src/ch/AutoPlayer.java
+++ src/ch/AutoPlayer.java
@@ -248,7 +248,7 @@ public final class AutoPlayer extends Panel implements Runnable {
                 this.player.getCube3D().setStickerColor(i, j, c);
             }
         }
-        String message = String.format("%d次执行完成，用时%.1fs", times, (System.nanoTime() - start) / 1000000000.0d);
+        String message = String.format("%d次执行完成，用时%.2fs", times, (System.nanoTime() - start) / 1000000000.0d);
         JOptionPane.showOptionDialog(this, message, "成功", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, AutoPlayer.this.infoIcon,
                 CommandParser.defaultOption, CommandParser.defaultOption[0]);
     }
